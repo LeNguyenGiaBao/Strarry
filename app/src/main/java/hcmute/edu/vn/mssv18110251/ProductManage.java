41d8106eb2ee211c207d782693c9389c39735046
Gia Bảo <lenguyengiabao46@gmail.com>
2021-05-21 18:33:19 +0700

    manage product

---
+++ app/src/main/java/hcmute/edu/vn/mssv18110251/ProductManage.java
@@ -0,0 +1,14 @@
+package hcmute.edu.vn.mssv18110251;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductManage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_manage);
+    }
+}
