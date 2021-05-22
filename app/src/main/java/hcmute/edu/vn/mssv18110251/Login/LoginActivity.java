3da5d36351c0800755b1f8af664ce7fdacb408c5
Gia Bảo <lenguyengiabao46@gmail.com>
2021-05-22 23:57:13 +0700

    login and register

--- app/src/main/java/hcmute/edu/vn/mssv18110251/Login/LoginActivity.java
+++ app/src/main/java/hcmute/edu/vn/mssv18110251/Login/LoginActivity.java
@@ -2,17 +2,29 @@ package hcmute.edu.vn.mssv18110251.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import hcmute.edu.vn.mssv18110251.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView go_to_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        go_to_register = findViewById(R.id.go_to_register);
+        go_to_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_register = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent_register);
+            }
+        });
 
     }
 }
--- app/src/main/java/hcmute/edu/vn/mssv18110251/Login/RegisterActivity.java
+++ app/src/main/java/hcmute/edu/vn/mssv18110251/Login/RegisterActivity.java
@@ -2,14 +2,28 @@ package hcmute.edu.vn.mssv18110251.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import hcmute.edu.vn.mssv18110251.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView go_to_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        go_to_login = findViewById(R.id.go_to_login);
+        go_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_login = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent_login);
+            }
+        });
     }
 }
